bdf8db2f429c1294386f41669215de2ce1e5584d
mahame Alfred <mahamealfred@gmail.com>
2022-11-07 14:42:49 +0200

    first commit46

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+const PRECACHE = 'Mobibanker-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+  'index.html',
+   './', 
+  'offline.html'
+];
+const self = this;
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+      caches.match(event.request)
+          .then(() => {
+              return fetch(event.request) 
+                  .catch(() => caches.match('offline.html'))
+          })
+  )
+});
