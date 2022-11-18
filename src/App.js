d0f488f31a284484d0eb18cc181bd2e233a638d4
mahame Alfred <mahamealfred@gmail.com>
2022-11-18 11:17:10 +0200

    ioo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
-import './App.css';
-
 function App() {
   let sum=1000000
   let ap='1 2 4 r'
   return (
     <div className="App">
       <h1>Hello from Alfred</h1>
-     {/* <p>{sum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</p> */}
      <p>{ap.replaceAll(/\s/g, '')}</p>
      <p>{sum.toLocaleString()}</p>
     </div>
